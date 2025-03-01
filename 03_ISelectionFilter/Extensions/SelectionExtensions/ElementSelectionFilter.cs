5da20c634d3912fb339a96c4000497b1a8693dbd
Furkan Turker <furkan.turker@flatlands.com.tr>
2025-03-01 12:09:17 +0300

    Enhance selection filtering by creating a new class that use delegate

---
+++ 03_ISelectionFilter/Extensions/SelectionExtensions/ElementSelectionFilter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+namespace _03_ISelectionFilter.Extensions.SelectionExtensions
+{
+    public class ElementSelectionFilter : ISelectionFilter
+    {
+        public readonly Func<Element,bool> _validateElement;
+    
+        public ElementSelectionFilter(Func<Element, bool> validateElement)
+        {
+            _validateElement = validateElement;
+        }
+
+        public bool AllowElement(Autodesk.Revit.DB.Element elem)
+        {
+            if(elem != null)
+            {
+                return _validateElement(elem);
+            }
+            return false;
+        }
+        public bool AllowReference(Autodesk.Revit.DB.Reference reference, Autodesk.Revit.DB.XYZ position)
+        {
+            return false;
+        }
+    }
+}
--- 03_ISelectionFilter/Main.cs
+++ 03_ISelectionFilter/Main.cs
@@ -8,19 +8,26 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.UI.Selection;
+using _03_ISelectionFilter.Extensions.SelectionExtensions;
 
 namespace _03_ISelectionFilter
 {
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
     public class Main : IExternalCommand
     {
-        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,ref string message, ElementSet elements)
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIApplication uiApp = commandData.Application;
-            Application app= uiApp.Application;
+            Application app = uiApp.Application;
             UIDocument uiDoc = uiApp.ActiveUIDocument;
             Document doc = uiDoc.Document;
 
-            return Autodesk.Revit.UI.Result.Succeeded; 
+            ElementSelectionFilter filter = new ElementSelectionFilter(elem => elem.Category.Id == new ElementId(BuiltInCategory.OST_Walls));
+
+            IList<Reference> reference = uiDoc.Selection.PickObjects(ObjectType.Element, filter, "Select a wall or walls");
+
+            return Autodesk.Revit.UI.Result.Succeeded;
         }
     }
 }
