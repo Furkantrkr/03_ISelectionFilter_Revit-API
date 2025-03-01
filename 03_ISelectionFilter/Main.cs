0b23c774d44a08121cd32b40815833c2f74d4e3f
Furkan Turker <furkan.turker@flatlands.com.tr>
2025-03-01 03:35:39 +0300

    Add project files.

---
+++ 03_ISelectionFilter/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+
+namespace _03_ISelectionFilter
+{
+    public class Main : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            Application app= uiApp.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            return Autodesk.Revit.UI.Result.Succeeded; 
+        }
+    }
+}
